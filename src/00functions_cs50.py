7e40c5e1283f35433698c4db739328c2b950d7cc
Dennis Mitchell <dennis.mitchell.x@gmail.com>
2023-01-01 08:37:10 -0500

    functions - cs50 harvard

--- src/00functions_cs50.py
+++ src/00functions_cs50.py
@@ -12,3 +12,26 @@ def print_menu():
     print("4. Open a folder")
 
 print_menu()
+
+# print
+# https://docs.python.org/3/library/functions.html?highlight=print#print
+# https://docs.python.org/3/library/functions.html#print
+
+# return values
+# variables
+# pseudocode
+# input accepts a string
+# str = string
+# print(*objects, sep=' ', end='\n', file=None, flush=False)
+# *objects = can take any number of objects
+# sep = separator
+# end = default value
+
+
+
+name: str = input("what name? ")
+print('hi', name, end='')
+print(f'hi {name} ', )
+
+# string methods
+# https://docs.python.org/3/library/stdtypes.html#string-methods
