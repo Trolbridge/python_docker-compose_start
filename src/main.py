8d2ed030612827d9ebf8cf9dd11a2e8f4fb4d7c9
Dennis Mitchell <dennis.mitchell.x@gmail.com>
2023-01-27 15:18:46 -0500

    python clean up code

--- src/main.py
+++ src/main.py
@@ -1,21 +1,21 @@
-my_name: str = "DEN"
-print("Hi", my_name )
-print("Hello", my_name )
-print("Hello", my_name, "World")
+import subprocess
+subprocess.run(["clear"])
 
-name: str = 'Bob'
+# VARIABLES
+# Statically typed python code
+# https://betterdatascience.com/python-statically-typed/
 age: int = 32
 rating: float = 7.9
 is_premium: bool = True
+my_name: str = "DEN"
+print("Hello", my_name, "World")
 
 def sum_numbers(a: int, b: int) -> int:
     return a + b
 
 print(sum_numbers(10, 5))
-print(sum_numbers(10, 5))
-print("It works!")
-print("It works!")
-print("It works!")
 
 # Use double quotes \"
-# escape characters: \, \n, \t, \r, \b, \f, \v, \0, \00, \01, \02, \0, \03, \0, \\, \', ,\", 
+# escape characters: \\, \n, \t, \r, \b, \f, \v, \0, \00, \01, \02, \0, \03, \0, \\, \', ,\", 
+
+print(repr('''Escape Characters: \\, \n, \t, \\", \', \01, \02, \03, \04, \05, ,\06, \07, \0'''))
