48fbc41ea4fa8a27207465c52942d130317bb68a
Dennis Mitchell <dennis.mitchell.x@gmail.com>
2023-01-27 13:59:26 -0500

    Everything updated to keep a running python container alive

--- src/main.py
+++ src/main.py
@@ -1,2 +1,19 @@
-my_name = "DEN"
+my_name: str = "DEN"
 print("Hi", my_name )
+print("Hello", my_name )
+print("Hello", my_name, "World")
+
+name: str = 'Bob'
+age: int = 32
+rating: float = 7.9
+is_premium: bool = True
+
+def sum_numbers(a: int, b: int) -> int:
+    return a + b
+
+print(sum_numbers(10, 5))
+print(sum_numbers(10, 5))
+print("It works!")
+
+# print(sum_numbers(10.3, 5))
+# print(sum_numbers('Bob', 'Mark'))
