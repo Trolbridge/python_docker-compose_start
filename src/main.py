9ada1df1805a2cd20eeacbecf476db7c1f941d76
Dennis Mitchell <dennis.mitchell.x@gmail.com>
2023-01-27 20:11:00 -0500

    python variables, data types, casting

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import subprocess
+import sys
 subprocess.run(["clear"])
 
 # VARIABLES
@@ -19,3 +20,18 @@ print(sum_numbers(10, 5))
 # escape characters: \\, \n, \t, \r, \b, \f, \v, \0, \00, \01, \02, \0, \03, \0, \\, \', ,\", 
 
 print(repr('''Escape Characters: \\, \n, \t, \\", \', \01, \02, \03, \04, \05, ,\06, \07, \0'''))
+
+print("max size of int", sys.maxsize)
+print("max size of float", sys.float_info.max)
+# floats are accurate to 15 digits
+
+cn_1: complex = 5 + 6j
+
+can_vote: bool = True
+
+# Casting
+print("Cast", type(int(rating)))
+print("Cast", type(str(rating)))
+print("Cast", type(chr(97))) # no chr types in python
+print("Cast", type(ord('a'))) # casting char to unicode
+print("Cast", type(float(2)))
